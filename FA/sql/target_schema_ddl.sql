d01f39d2ae43b988a4b1a0c970db0eca8515a4d2
Sergey Garanin <sa.garanin@gmail.com>
2020-07-09 12:05:51 +0300

    + constraint

--- FA/sql/target_schema_ddl.sql
+++ FA/sql/target_schema_ddl.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS celonis_etl.table_metadata (
 	src_query text,
 	src_schema varchar,
 	src_table varchar,
-	fields varchar,
+	fields varchar NOT NULL,
 	key_field varchar NOT NULL,
 	tgt_schema varchar NOT NULL DEFAULT 'celonis_etl',
 	tgt_table varchar NOT NULL,
@@ -16,6 +16,7 @@ CREATE TABLE IF NOT EXISTS celonis_etl.table_metadata (
 	author varchar NOT NULL,
 	udpated timestamp NOT NULL DEFAULT now(),
 	CONSTRAINT load_type_constraint CHECK ((load_type = ANY (ARRAY['f'::bpchar, 'i'::bpchar]))),
+	CONSTRAINT query_schema_table_constraint CHECK (src_query is not null or (src_schema is not null and src_table is not null)),
 	CONSTRAINT tables_md_pk PRIMARY KEY (id)
 );
 
